0f30390acdc72aaa05e9b97ae17ec949dcd711b0
Ahydul <ahydul1@gmail.com>
2024-05-09 19:12:40 +0200

    Remove debug logging

--- app/src/main/java/com/example/tfg/ui/components/common/Headers.kt
+++ app/src/main/java/com/example/tfg/ui/components/common/Headers.kt
@@ -1,6 +1,5 @@
 package com.example.tfg.ui.components.common
 
-import android.util.Log
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
@@ -21,26 +20,26 @@ fun MainHeader(modifier: Modifier = Modifier) {
             dismissedColor = colorResource(id = R.color.primary_background)
         ) {
             CustomFilledIconButton(
-                onClick = {  Log.d("navigation","header1")  },
+                onClick = {  },
                 imageVector = ImageVector.vectorResource(id = R.drawable.paint_brush),
                 contentDescription = "Click me for menu",
                 color = Color.Red,
             )
             CustomFilledIconButton(
-                onClick = { Log.d("navigation","header2") },
+                onClick = {  },
                 imageVector = ImageVector.vectorResource(id = R.drawable.paint_brush),
                 contentDescription = "Click me for menu",
                 color = Color.Green
             )
             CustomFilledIconButton(
-                onClick = { Log.d("navigation","header3") },
+                onClick = {  },
                 imageVector = ImageVector.vectorResource(id = R.drawable.paint_brush),
                 contentDescription = "Click me for menu",
                 color = Color.Magenta
             )
         }
         CustomIconButton(
-            onClick = { Log.d("navigation","config") },
+            onClick = {  },
             imageVector = ImageVector.vectorResource(id = R.drawable.gear),
             contentDescription = "Click me for menu",
         )
