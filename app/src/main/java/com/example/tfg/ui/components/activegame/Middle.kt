cf5b3e90d5e6e61dbec69c38ae61f5530fdc7a72
Ahydul <ahydul1@gmail.com>
2024-05-12 12:38:50 +0200

    Add debug to check recomposition

--- app/src/main/java/com/example/tfg/ui/components/activegame/Middle.kt
+++ app/src/main/java/com/example/tfg/ui/components/activegame/Middle.kt
@@ -3,7 +3,6 @@ package com.example.tfg.ui.components.activegame
 import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentRecomposeScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.tfg.state.ActiveGameViewModel
@@ -13,7 +12,7 @@ fun MiddleSection(
     viewModel: ActiveGameViewModel,
     modifier: Modifier = Modifier
 ) {
-    Log.d("TAG", "MIDDLEcurrentRecomposeScope $currentRecomposeScope")
+    Log.d("recomposition", "MIDDLE recomposition")
 
     Box (
         contentAlignment = Alignment.Center,
