51ee9b0e9bc16bc2cd0eed19da472a5a75436daa
Ahydul <ahydul1@gmail.com>
2024-05-12 19:53:19 +0200

    Add difficulty input

--- app/src/main/java/com/example/tfg/common/Difficulty.kt
+++ app/src/main/java/com/example/tfg/common/Difficulty.kt
@@ -1,7 +1,27 @@
 package com.example.tfg.common
 
+import android.content.Context
+import com.example.tfg.R
+
 enum class Difficulty {
     EASY,
     MEDIUM,
-    HARD,
+    HARD;
+
+    fun toString(context: Context): String {
+        return when(this){
+            EASY -> context.getString(R.string.easy)
+            MEDIUM -> context.getString(R.string.medium)
+            HARD -> context.getString(R.string.hard)
+        }
+    }
+
+    companion object {
+        fun get(value: String): Difficulty {
+            return Difficulty.entries.find { it.name.lowercase() == value.lowercase() }!!
+        }
+        fun get(index: Int): Difficulty {
+            return Difficulty.entries[index]
+        }
+    }
 }
