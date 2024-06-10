a36887ae5c517ef14c45cfd0b833f78b670b1a1c
Ahydul <ahydul1@gmail.com>
2024-06-10 18:55:10 +0200

    Add rules page

--- app/src/main/java/com/example/tfg/games/common/Games.kt
+++ app/src/main/java/com/example/tfg/games/common/Games.kt
@@ -1,5 +1,7 @@
 package com.example.tfg.games.common
 
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.buildAnnotatedString
 import com.example.tfg.common.enums.Games2
 
 enum class Games(val title: String) {
@@ -12,6 +14,22 @@ enum class Games(val title: String) {
     fun toGames2(): Games2 {
         return when(this){
             HAKYUU -> Games2.HAKYUU
+        }
+    }
+
+    fun getRules(): AnnotatedString {
+        return when(this){
+            HAKYUU -> buildAnnotatedString {
+                    append("1. A number must be entered in each field of the diagram.\n\n")
+                    append("2. In every area of N fields, every number from the range 1~N must appear exactly once.\n\n")
+                    append("3. If two fields in a row or column contain the same number Z, there must be at least Z fields with different numbers between these two fields.\n\n")
+
+                    append("Rules from: ")
+
+                    pushStringAnnotation(tag = "web", annotation = "https://www.janko.at/Raetsel/Hakyuu/index.htm")
+                    append("https://www.janko.at/Raetsel/Hakyuu/index.htm")
+                    pop()
+                }
         }
     }
 
