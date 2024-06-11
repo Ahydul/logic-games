3e04e06e0891cc3da02283cef7de7bdf306116c1
Ahydul <ahydul1@gmail.com>
2024-06-11 18:48:57 +0200

    Prepare to build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,11 +11,11 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.tfg"
+        applicationId = "com.ahydul.logic_games"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -112,5 +112,5 @@ dependencies {
 
     val aboutLib = "11.2.1"
     implementation("com.mikepenz:aboutlibraries:$aboutLib") // Add this line
-    implementation ("com.mikepenz:aboutlibraries-compose-m3:$aboutLib")
+    implementation("com.mikepenz:aboutlibraries-compose-m3:$aboutLib")
 }
