779f78653cae0f8f2f58e2768c63768400f3aa8a
Ahydul <ahydul1@gmail.com>
2024-06-13 19:26:29 +0200

    Improve header layout

--- app/src/main/java/com/example/tfg/ui/components/mainactivity/HomeScreen.kt
+++ app/src/main/java/com/example/tfg/ui/components/mainactivity/HomeScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.tfg.state.MainViewModel
 import com.example.tfg.ui.components.common.MainHeader
 
@@ -19,6 +21,7 @@ fun HomeScreen(
     val mod = Modifier.fillMaxWidth()
     Column(modifier = modifier
         .fillMaxHeight()
+        .padding(top = 8.dp, start = 6.dp, end = 6.dp)
     ) {
         MainHeader(viewModel = viewModel, modifier = modifier.weight(1.3f))
         Spacer(modifier = mod.weight(8f))
