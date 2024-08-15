120d25f9e592c4a0b6e774e1fa461fa8181e2c5d
Ahydul <ahydul1@gmail.com>
2024-08-15 18:32:21 +0200

    Add configurable allowed operations to Kendoku

--- app/src/main/java/com/example/tfg/games/kendoku/KendokuOperation.kt
+++ app/src/main/java/com/example/tfg/games/kendoku/KendokuOperation.kt
@@ -4,5 +4,10 @@ enum class KendokuOperation {
     SUM,
     SUBTRACT,
     MULTIPLY,
-    DIVIDE
+    DIVIDE,
+    SUM_UNKNOWN,
+    SUBTRACT_UNKNOWN,
+    MULTIPLY_UNKNOWN,
+    DIVIDE_UNKNOWN,
+    ANY
 }
