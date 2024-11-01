80cb8d826d47feba37a097fd88c09123f776f18b
Ahydul <ahydul1@gmail.com>
2024-11-01 23:22:18 +0100

    Refactor KendokuScore

--- app/src/main/java/com/example/tfg/games/kendoku/KendokuScore.kt
+++ app/src/main/java/com/example/tfg/games/kendoku/KendokuScore.kt
@@ -28,47 +28,95 @@ private val difficultyValues = DifficultyValues(
     MAX_MASTER_BRUTE_FORCES = 3,
 )
 
-class KendokuScore(score: Int = 0, bruteForce: Int = 0) : Score(score, bruteForce, difficultyValues) {
+class KendokuScore(
+    private var nakedPairs: Int = 0,
+    private var nakedTriples: Int = 0,
+    private var hiddenSingles: Int = 0,
+    private var hiddenPairs: Int = 0,
+    private var hiddenTriples: Int = 0,
+    private var mainCombinationReduces: Int = 0,
+    private var cageUnitOverlapsType1: Int = 0,
+    private var cageUnitOverlapsType2: Int = 0,
+    private var biValueAttacks: Int = 0,
+    private var inniesOuties: Int = 0,
+    private var xWings: Int = 0,
+    private var simpleColoring: Int = 0,
+    score: Int = 0,
+    bruteForce: Int = 0,
+) : Score(score, bruteForce, difficultyValues) {
+
+    override fun toString(): String {
+        return "$nakedPairs, $nakedTriples, $hiddenSingles, $hiddenPairs, $hiddenTriples, $mainCombinationReduces, $cageUnitOverlapsType1, $cageUnitOverlapsType2, $biValueAttacks, $inniesOuties, $xWings, $simpleColoring"
+    }
+
+    override fun add(s: Score?) {
+        super.add(s)
+        if (s == null) return
+        val s = (s as KendokuScore)
+        nakedPairs += s.nakedPairs
+        nakedTriples += s.nakedTriples
+        hiddenSingles += s.hiddenSingles
+        hiddenPairs += s.hiddenPairs
+        hiddenTriples += s.hiddenTriples
+        cageUnitOverlapsType1 += s.cageUnitOverlapsType1
+        cageUnitOverlapsType2 += s.cageUnitOverlapsType2
+        mainCombinationReduces += s.mainCombinationReduces
+        biValueAttacks += s.biValueAttacks
+        inniesOuties += s.inniesOuties
+        simpleColoring += s.simpleColoring
+    }
 
     //TODO: Complete score
     fun addNakedPairs(numPairs: Int) {
         score += numPairs
+        nakedPairs += numPairs
     }
 
     fun addNakedTriples(numTriples: Int) {
         score += numTriples
+        nakedTriples += numTriples
     }
 
     fun addHiddenSPT(numSPT: IntArray) {
         numSPT.forEach { score += it }
+        hiddenSingles += numSPT[0]
+        hiddenPairs += numSPT[1]
+        hiddenTriples += numSPT[2]
     }
 
     fun addCombinations(numValuesRemoved: Int) {
         score += numValuesRemoved
+        mainCombinationReduces += numValuesRemoved
     }
 
     fun addCageUnitOverlapType2(numCUO: Int) {
         score += numCUO
+        cageUnitOverlapsType2 += numCUO
     }
 
     fun addBiValueAttack(numChanges: Int) {
         score += numChanges
+        biValueAttacks += numChanges
     }
 
     fun addInniesOuties(numInniesOuties: Int) {
         score += numInniesOuties
+        inniesOuties += numInniesOuties
     }
 
     fun addXWings(numXWings: Int) {
         score += numXWings
+        xWings += numXWings
     }
 
     fun addColoring(numColoring: Int) {
         score += numColoring
+        simpleColoring += numColoring
     }
 
     fun addCageUnitOverlapType1(numCUO: Int) {
         score += numCUO
+        cageUnitOverlapsType1 += numCUO
     }
 
     override fun serialize(): JsonElement {
